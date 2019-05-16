d92232f50ddc55b2e220206373af66f033dfd813
Meredith Espinosa <goclonefilms@gmail.com>
2019-05-16 12:07:04 -0700

    update to 1.14.1, api 0.3.0

--- src/main/java/com/raphydaphy/crochet/network/PacketHandler.java
+++ src/main/java/com/raphydaphy/crochet/network/PacketHandler.java
@@ -29,7 +29,7 @@ public class PacketHandler
 
 		if (netHandler != null)
 		{
-			netHandler.getClientConnection().sendPacket(new CustomPayloadC2SPacket(packet.getID(), buf));
+			netHandler.getClientConnection().send(new CustomPayloadC2SPacket(packet.getID(), buf));
 		}
 	}
 
