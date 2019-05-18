77241b579741bef5c55e6822324d217ea38f4e53
Raph Hennessy <raph.hennessy@gmail.com>
2019-05-18 17:48:51 +1000

    Sync entity data when dimension changes

--- src/main/java/com/raphydaphy/crochet/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/raphydaphy/crochet/mixin/ServerPlayerEntityMixin.java
@@ -1,13 +1,18 @@
 package com.raphydaphy.crochet.mixin;
 
 import com.raphydaphy.crochet.data.DataHolder;
+import com.raphydaphy.crochet.data.PlayerData;
 import com.raphydaphy.crochet.network.PacketHandler;
 import com.raphydaphy.crochet.network.PlayerDataUpdatePacket;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityData;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin implements DataHolder
@@ -17,6 +22,7 @@ public abstract class ServerPlayerEntityMixin implements DataHolder
 	@Inject(at = @At("HEAD"), method = "copyFrom")
 	private void onPlayerClone(ServerPlayerEntity playerEntity, boolean keepEverything, CallbackInfo info) // copyFrom
 	{
+		System.out.println("ok dam they cloned");
 		this.setAllAdditionalData(((DataHolder) playerEntity).getAllAdditionalData());
 		markAdditionalDataDirty();
 	}
@@ -29,6 +35,11 @@ public abstract class ServerPlayerEntityMixin implements DataHolder
 			additionalDataNeedsSync = false;
 			PacketHandler.sendToClient(new PlayerDataUpdatePacket(this.getAllAdditionalData()), (ServerPlayerEntity) (Object) this);
 		}
+	}
+
+	@Inject(at = @At("RETURN"), method="changeDimension")
+	private void changeDimension(DimensionType dimensionType_1, CallbackInfoReturnable<Entity> info) {
+		PlayerData.markDirty((ServerPlayerEntity)(Object)this);
 	}
 
 	@Override
